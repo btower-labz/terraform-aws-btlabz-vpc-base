f3d1ee6784d65222d9472ffc8bc76eb51ce72749
BTower Labz <labz@btower.net>
2018-03-02 15:31:56 +0000

    ...

--- main.tf
+++ main.tf
@@ -23,17 +23,6 @@ resource "aws_internet_gateway" "main" {
   )}"
 }
 
-# TODO: Use default route table
-#resource "aws_route_table" "main" {
-#  vpc_id = "${aws_vpc.main.id}"
-#  tags = "${merge(
-#    var.tags,
-#    map(
-#      "Name", "${var.rt_name}"
-#    )
-#  )}"
-#}
-
 resource "aws_default_route_table" "main" {
   default_route_table_id = "${aws_vpc.main.default_route_table_id}"
 
@@ -50,9 +39,3 @@ resource "aws_route" "main" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.main.id}"
 }
-
-#resource "aws_main_route_table_association" "main" {
-#  vpc_id         = "${aws_vpc.main.id}"
-#  route_table_id = "${aws_route_table.main.id}"
-#}
-
