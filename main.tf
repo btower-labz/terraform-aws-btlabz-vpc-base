9d24e2a834e96a58d773bab1d3e9213cd80fefc3
BTower Labz <labz@btower.net>
2020-08-01 11:36:01 +0000

    upgrated to 12x

--- main.tf
+++ main.tf
@@ -1,41 +1,42 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "${var.cidr}"
+  cidr_block           = var.cidr
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = "${merge(
+  tags = merge(
     var.tags,
-    map(
-      "Name", "${var.vpc_name}"
-    )
-  )}"
+    {
+      "Name" = var.vpc_name
+    },
+  )
 }
 
 resource "aws_internet_gateway" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-  tags = "${merge(
+  tags = merge(
     var.tags,
-    map(
-      "Name", "${var.igw_name}"
-    )
-  )}"
+    {
+      "Name" = var.igw_name
+    },
+  )
 }
 
 resource "aws_default_route_table" "main" {
-  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+  default_route_table_id = aws_vpc.main.default_route_table_id
 
-  tags = "${merge(
+  tags = merge(
     var.tags,
-    map(
-      "Name", "${var.rt_name}"
-    )
-  )}"
+    {
+      "Name" = var.rt_name
+    },
+  )
 }
 
 resource "aws_route" "main" {
-  route_table_id         = "${aws_default_route_table.main.id}"
+  route_table_id         = aws_default_route_table.main.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.main.id}"
+  gateway_id             = aws_internet_gateway.main.id
 }
+
--- variables.tf
+++ variables.tf
@@ -1,28 +1,29 @@
 variable "vpc_name" {
   description = "VPC name. This will be uses as the 'Name' tag value."
-  type        = "string"
+  type        = string
   default     = "main-vpc"
 }
 
 variable "cidr" {
   description = "VPC's CIDR range."
-  type        = "string"
+  type        = string
 }
 
 variable "rt_name" {
   description = "VPC's main public route table name."
-  type        = "string"
+  type        = string
   default     = "public-rt"
 }
 
 variable "tags" {
   description = "Additional tags."
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "igw_name" {
   description = "Internet gateway name. This will be uses as the 'Name' tag value."
-  type        = "string"
+  type        = string
   default     = "main-igw"
 }
+
